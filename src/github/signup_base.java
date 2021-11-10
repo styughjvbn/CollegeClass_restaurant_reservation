ed84091bbbee94f62fbf895004b3204ec898fdc2
styughjvbn <3338761@naver.com>
2021-11-10 21:07:28 +0900

    사진 상대경로로 수정

--- src/github/signup_base.java
+++ src/github/signup_base.java
@@ -69,7 +69,7 @@ public class signup_base extends JPanel {
 		btnNewButton = new JButton("");
 		btnNewButton.setBorder(null);
 		btnNewButton.setBackground(Color.WHITE);
-		btnNewButton.setIcon(new ImageIcon("C:\\Users\\33387\\Desktop\\2\uD559\uB144 2\uD559\uAE30\\\uC624\uD508\uC18C\uC2A4\uD504\uB85C\uC81D\uD2B8\\restaurant_reservation\\image\\back.png"));
+		btnNewButton.setIcon(new ImageIcon("image/back.png"));
 		btnNewButton.setBounds(12, 10, 61, 50);
 		add(btnNewButton);
 		
