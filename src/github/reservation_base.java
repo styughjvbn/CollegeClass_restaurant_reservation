1f220ccbaa9aa6d44775682022730b80d46e3133
alswnsp411 <alswnsp411@naver.com>
2021-11-26 12:55:50 +0900

    매장 검색 거의 완성

--- src/github/reservation_base.java
+++ src/github/reservation_base.java
@@ -44,6 +44,15 @@ public class reservation_base extends JPanel {
 	private JTextField holyday;
 	private DTO_shop cnt_shop;
 	private reservation_payment payment;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_6;
+	private JLabel lblNewLabel_7;
+	private JLabel lblNewLabel_8;
+	private JLabel lblNewLabel_9;
+	private JLabel lblNewLabel_10;
 	/**
 	 * Create the panel.
 	 */
@@ -62,7 +71,7 @@ public class reservation_base extends JPanel {
 		add(payment,"payment");
 		
 		JLabel lblNewLabel = new JLabel();
-		lblNewLabel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		lblNewLabel.setBorder(new LineBorder(new Color(200, 184, 164), 1, true));
 		lblNewLabel.setBounds(446, 40, 782, 606);
 		panel.add(lblNewLabel);
 
@@ -70,7 +79,7 @@ public class reservation_base extends JPanel {
 		back.setBorder(null);
 		back.setBackground(new Color(226,221,215));
 		back.setIcon(new ImageIcon("image/back.png"));
-		back.setBounds(33, 40, 53, 43);
+		back.setBounds(33, 40, 45, 43);
 		panel.add(back);
 		
 		JPanel panel_1 = new JPanel();
@@ -80,13 +89,18 @@ public class reservation_base extends JPanel {
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(226,221,215));
+		panel_2.setBounds(12, 124, 389, 422);
+		panel_1.add(panel_2);
+		panel_2.setLayout(null);
+		
 		shop_name = new JTextField();
 		shop_name.setForeground(new Color(120, 108, 100));
 		shop_name.setBackground(new Color(226,221,215));
 		shop_name.setBorder(null);
-		shop_name.setFont(new Font("±¼¸²", Font.BOLD, 17));
-		shop_name.setText("\uC0C1\uB2F9\uD68C\uAD00");
-		shop_name.setBounds(29, 149, 360, 26);
+		shop_name.setFont(new Font("±¼¸²", Font.BOLD, 20));
+		shop_name.setBounds(29, 149, 358, 26);
 		panel_1.add(shop_name);
 		shop_name.setEditable(false);
 		shop_name.setColumns(10);
@@ -95,18 +109,17 @@ public class reservation_base extends JPanel {
 		shop_tel.setBorder(null);
 		shop_tel.setBackground(new Color(226,221,215));
 		shop_tel.setFont(new Font("±¼¸²", Font.PLAIN, 15));
-		shop_tel.setText("043-255-1967");
-		shop_tel.setBounds(29, 228, 360, 21);
+		shop_tel.setBounds(68, 226, 321, 21);
 		panel_1.add(shop_tel);
 		shop_tel.setEditable(false);
 		shop_tel.setColumns(10);
 		
 		shop_distance = new JTextField();
+		shop_distance.setForeground(new Color(236, 99, 94));
 		shop_distance.setBorder(null);
 		shop_distance.setBackground(new Color(226,221,215));
 		shop_distance.setFont(new Font("±¼¸²", Font.PLAIN, 15));
-		shop_distance.setText("417");
-		shop_distance.setBounds(29, 290, 360, 21);
+		shop_distance.setBounds(151, 297, 236, 21);
 		panel_1.add(shop_distance);
 		shop_distance.setEditable(false);
 		shop_distance.setColumns(10);
@@ -115,8 +128,7 @@ public class reservation_base extends JPanel {
 		shop_adress.setBorder(null);
 		shop_adress.setBackground(new Color(226,221,215));
 		shop_adress.setFont(new Font("±¼¸²", Font.PLAIN, 15));
-		shop_adress.setText("\uCDA9\uBD81 \uCCAD\uC8FC\uC2DC \uC0C1\uB2F9\uAD6C \uBD81\uBB38\uB85C2\uAC00 27-5");
-		shop_adress.setBounds(29, 259, 360, 21);
+		shop_adress.setBounds(68, 268, 319, 21);
 		panel_1.add(shop_adress);
 		shop_adress.setEditable(false);
 		shop_adress.setColumns(10);
@@ -126,36 +138,38 @@ public class reservation_base extends JPanel {
 		shop_category.setBackground(new Color(226,221,215));
 		shop_category.setForeground(Color.GRAY);
 		shop_category.setFont(new Font("±¼¸²", Font.PLAIN, 15));
-		shop_category.setText("\uC74C\uC2DD\uC810>\uD55C\uC2DD>\uC721\uB958, \uACE0\uAE30>\uC0BC\uACB9\uC0B4");
-		shop_category.setBounds(29, 185, 360, 21);
+		shop_category.setBounds(29, 185, 358, 21);
 		panel_1.add(shop_category);
 		shop_category.setEditable(false);
 		shop_category.setColumns(10);
 		
 		open_time = new JTextField();
 		open_time.setBorder(null);
-		open_time.setBackground(new Color(226,221,215));
+		open_time.setBackground(new Color(228,221,215));
 		open_time.setFont(new Font("±¼¸²", Font.PLAIN, 15));
-		open_time.setText("8:00 ~22:00");
-		open_time.setBounds(29, 386, 329, 21);
+		open_time.setBounds(142, 337, 249, 21);
 		panel_1.add(open_time);
 		open_time.setColumns(10);
 		
 		holyday = new JTextField();
+		holyday.setFont(new Font("±¼¸²", Font.PLAIN, 15));
 		holyday.setBorder(null);
-		holyday.setBackground(new Color(226,221,215));
-		holyday.setText("\uB9E4\uC8FC \uC218\uC694\uC77C");
-		holyday.setBounds(29, 417, 329, 21);
+		holyday.setBackground(new Color(228,221,215));
+		holyday.setBounds(142, 365, 249, 21);
 		panel_1.add(holyday);
 		holyday.setColumns(10);
 		
 		JButton btnNewButton_1 = new JButton("\uC608\uC57D\uD558\uAE30");
-		btnNewButton_1.setBounds(45, 464, 321, 40);
+		btnNewButton_1.setForeground(new Color(120, 108, 100));
+		btnNewButton_1.setBackground(new Color(200, 184, 164));
+		btnNewButton_1.setBorder(new LineBorder(new Color(200, 184, 164), 2, true));
+		btnNewButton_1.setFont(new Font("±¼¸²", Font.BOLD, 20));
+		btnNewButton_1.setBounds(45, 458, 321, 46);
 		panel_1.add(btnNewButton_1);
 		textField = new JTextField();
 		textField.setForeground(new Color(120, 108, 100));
-		textField.setBackground(new Color(200, 184, 164));
-		textField.setBounds(29, 26, 154, 39);
+		textField.setBackground(new Color(226,221,215));
+		textField.setBounds(36, 26, 140, 40);
 		panel_1.add(textField);
 		textField.setFont(new Font("±¼¸²", Font.BOLD, 17));
 		textField.setText("\uC704\uCE58 \uAC80\uC0C9");
@@ -166,7 +180,7 @@ public class reservation_base extends JPanel {
 			public void focusGained(FocusEvent e) {
 				if (textField.getText().equals("À§Ä¡ °Ë»ö")) {
 					textField.setText("");
-					textField.setForeground(new Color(120, 108, 100));
+					textField.setForeground(Color.BLACK);
 				}
 
 			}
@@ -182,10 +196,10 @@ public class reservation_base extends JPanel {
 		
 		textField_1 = new JTextField();
 		textField_1.setForeground(new Color(120, 108, 100));
-		textField_1.setBackground(new Color(200, 184, 164));
+		textField_1.setBackground(new Color(226,221,215));
 		textField_1.setFont(new Font("±¼¸²", Font.BOLD, 17));
 		textField_1.setText("\uD0A4\uC6CC\uB4DC \uAC80\uC0C9");
-		textField_1.setBounds(194, 26, 150, 39);
+		textField_1.setBounds(192, 26, 155, 40);
 		panel_1.add(textField_1);
 		textField_1.setBorder(new LineBorder(new Color(120, 108, 100), 0, true));
 		textField_1.setColumns(10);
@@ -194,7 +208,7 @@ public class reservation_base extends JPanel {
 			public void focusGained(FocusEvent e) {
 				if (textField_1.getText().equals("Å°¿öµå °Ë»ö")) {
 					textField_1.setText("");
-					textField_1.setForeground(new Color(120, 108, 100));
+					textField_1.setForeground(Color.BLACK);
 				}
 
 			}
@@ -210,8 +224,8 @@ public class reservation_base extends JPanel {
 		
 		JButton btnNewButton = new JButton("");
 		btnNewButton.setBorder(null);
-		btnNewButton.setIcon(new ImageIcon("C:\\Users\\kmj\\Documents\\GitHub\\restaurant_reservation\\image\\searchiconn.png"));
-		btnNewButton.setBounds(344, 22, 43, 43);
+		btnNewButton.setIcon(new ImageIcon("image/search_icon1.png"));
+		btnNewButton.setBounds(347, 26, 42, 40);
 		panel_1.add(btnNewButton);
 		JComboBox<String> comboBox = new JComboBox();
 		comboBox.setBorder(new LineBorder(new Color(200, 184, 164), 1, true));
@@ -220,16 +234,68 @@ public class reservation_base extends JPanel {
 		comboBox.setBounds(29, 75, 358, 39);
 		panel_1.add(comboBox);
 		
+		lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("image/call_icon.png"));
+		lblNewLabel_1.setBounds(29, 221, 40, 26);
+		panel_1.add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon("image/location_icon.png"));
+		lblNewLabel_2.setBounds(25, 268, 30, 40);
+		panel_1.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon("image/time_icon.png"));
+		lblNewLabel_3.setBounds(29, 340, 32, 30);
+		panel_1.add(lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setFont(new Font("±¼¸²", Font.PLAIN, 15));
+		lblNewLabel_4.setBounds(68, 297, 81, 21);
+		panel_1.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon("image/section_icon.png"));
+		lblNewLabel_5.setBounds(175, 26, 5, 40);
+		panel_1.add(lblNewLabel_5);
+		
+		lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setIcon(new ImageIcon("image/hsection_icon.png"));
+		lblNewLabel_6.setBounds(25, 208, 361, 15);
+		panel_1.add(lblNewLabel_6);
+		
+		lblNewLabel_7 = new JLabel("");
+		lblNewLabel_7.setIcon(new ImageIcon("image/hsection_icon.png"));
+		lblNewLabel_7.setBounds(25, 248, 361, 15);
+		panel_1.add(lblNewLabel_7);
+		
+		lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon("image/hsection_icon.png"));
+		lblNewLabel_8.setBounds(25, 315, 361, 15);
+		panel_1.add(lblNewLabel_8);
+		
+		lblNewLabel_9 = new JLabel("\uC601\uC5C5 \uC2DC\uAC04");
+		lblNewLabel_9.setFont(new Font("±¼¸²", Font.BOLD, 15));
+		lblNewLabel_9.setBounds(68, 337, 69, 21);
+		panel_1.add(lblNewLabel_9);
+		
+		lblNewLabel_10 = new JLabel("\uC815\uAE30 \uD734\uBB34");
+		lblNewLabel_10.setForeground(new Color(236, 99, 94));
+		lblNewLabel_10.setFont(new Font("±¼¸²", Font.BOLD, 15));
+		lblNewLabel_10.setBounds(68, 365, 69, 21);
+		panel_1.add(lblNewLabel_10);
+		
 		
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JComboBox cb = (JComboBox) e.getSource();
 				int index = cb.getSelectedIndex();
 				if (index > -1) {
+					panel_2.setVisible(false);
 					shop_name.setText(list1.get(index)[0]);
 					cnt_shop=DAO.get_shop_info(shop_name.getText());
 					shop_tel.setText(list1.get(index)[1]);
-					shop_distance.setText(list1.get(index)[2]);
+					shop_distance.setText(list1.get(index)[2]+"m");
 					shop_adress.setText(list1.get(index)[3]);
 					shop_category.setText(list1.get(index)[4]);
 					if(cnt_shop!=null) {//ÀÔÁ¡µÈ Á¡Æ÷¶ó¸é ¿µ¾÷½Ã°£°ú °øÈÞÀÏ Ãâ·Â
@@ -266,7 +332,9 @@ public class reservation_base extends JPanel {
 					create_image.create_map_image(machine.get_center_x(), machine.get_center_y(), list1.get(index)[5],list1.get(index)[6], shop, list1.get(index)[0]);
 					lblNewLabel.setIcon(new ImageIcon("image/shop/" + list1.get(index)[0] + ".png"));
 					shop = list1.get(index)[0];
+					
 				}
+				
 			}
 		});
 		btnNewButton.addActionListener(new ActionListener() {
